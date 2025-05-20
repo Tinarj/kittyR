371f724ed62fc35ca810eeb58f9b300fd29f2602
Tina Rashid Jafari <tina.rashidjafari@gmail.com>
2025-05-20 10:48:58 +1000

    Initial commit

---
+++ R/kitty.R
@@ -0,0 +1,13 @@
+#' Show a Cute Cat
+#'
+#' This function prints an ASCII art kitten to cheer you up.
+#'
+#' @export
+kitty <- function() {
+  cat("
+     /\\_/\\
+    ( o.o )
+     > ^ <
+  ")
+}
+
